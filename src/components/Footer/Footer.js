e237b7d54a40625e08c30734b2fa15f16c4708f5
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-11-26 20:30:48 +0100

    Remove div wrapper from Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,31 +8,29 @@ const Footer = () => {
     return (
         <div>
             {
-                 width < breakPoint ? 
-                 (<div>
-                     <footer className='footer'>
-                         <div className='footer__redsysWrapper'>
-                             <p>Powered by</p> 
-                             <img alt='Redsys logo' src={Redsys} />
-                         </div>
-                         <div className='footer__linksWrapper'>
-                             <a className='footer__linksWrapper--terms' href='https://en.wikipedia.org/wiki/Contractual_term'>Terms</a>
-                             <a className='footer__linksWrapper--priv' href='https://en.wikipedia.org/wiki/Privacy'>Privacy</a>
-                         </div>
-                     </footer>
-             </div>)
+                width < breakPoint ? 
+                (<footer className='footer'>
+                    <div className='footer__redsysWrapper'>
+                        <p>Powered by</p> 
+                        <img alt='Redsys logo' src={Redsys} />
+                    </div>
+                    <div className='footer__linksWrapper'>
+                        <a className='footer__linksWrapper--terms' href='https://en.wikipedia.org/wiki/Contractual_term'>Terms</a>
+                        <a className='footer__linksWrapper--priv' href='https://en.wikipedia.org/wiki/Privacy'>Privacy</a>
+                    </div>
+                </footer>)
              
              :
              
              (<footer className='footer footer--desktop'>
                  <div className='footer__redsysWrapper'>
-                     <p>Powered by</p> 
-                     <img alt='Redsys logo' src={Redsys} />
+                    <p>Powered by</p> 
+                    <img alt='Redsys logo' src={Redsys} />
                  </div>
                  <pre> | </pre>
                  <div className='footer__linksWrapper'>
-                     <a className='footer__linksWrapper--terms' href='https://en.wikipedia.org/wiki/Contractual_term'>Terms</a>
-                     <a className='footer__linksWrapper--priv' href='https://en.wikipedia.org/wiki/Privacy'>Privacy</a>
+                    <a className='footer__linksWrapper--terms' href='https://en.wikipedia.org/wiki/Contractual_term'>Terms</a>
+                    <a className='footer__linksWrapper--priv' href='https://en.wikipedia.org/wiki/Privacy'>Privacy</a>
                  </div>
              </footer>) 
             }
