250964ba3fd60e26903206007f5dcc0404be79fd
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-11-26 18:25:14 +0100

    Add flexbox desktop view to Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,48 @@
 
 import Redsys from '../../assets/images/redsys_logo.jpg'
+import useViewPort from '../../hooks/UseViewPort';
 
 const Footer = () => {
+    const  {width}  = useViewPort();
+    const breakPoint = 1024;
     return (
-        <footer className='footer'>
-            <div className='footer__redsysWrapper'>
-                <p>Powered by</p> 
-                <img alt='Redsys logo' src={Redsys} />
-            </div>
-            
-            <div className='footer__linksWrapper'>
-                <a className='footer__linksWrapper--terms' href='https://en.wikipedia.org/wiki/Contractual_term'>Terms</a>
-                <a className='footer__linksWrapper--priv' href='https://en.wikipedia.org/wiki/Privacy'>Privacy</a>
-            </div>
-        </footer>
+        <div>
+            {
+                 width < breakPoint ? 
+                 (<div>
+                     <footer className='footer'>
+                         <div className='footer__redsysWrapper'>
+                             <p>Powered by</p> 
+                             <img alt='Redsys logo' src={Redsys} />
+                         </div>
+                         <div className='footer__linksWrapper'>
+                             <a className='footer__linksWrapper--terms' href='https://en.wikipedia.org/wiki/Contractual_term'>Terms</a>
+                             <a className='footer__linksWrapper--priv' href='https://en.wikipedia.org/wiki/Privacy'>Privacy</a>
+                         </div>
+                     </footer>
+             </div>)
+             
+             :
+             
+             (<footer className='footer footer--desktop'>
+                 <div className='footer__redsysWrapper'>
+                     <p>Powered by</p> 
+                     <img alt='Redsys logo' src={Redsys} />
+                 </div>
+                 <pre> | </pre>
+                 <div className='footer__linksWrapper'>
+                     <a className='footer__linksWrapper--terms' href='https://en.wikipedia.org/wiki/Contractual_term'>Terms</a>
+                     <a className='footer__linksWrapper--priv' href='https://en.wikipedia.org/wiki/Privacy'>Privacy</a>
+                 </div>
+             </footer>) 
+            }
+        </div>
     )
+         
+        
+        
+        
+    
 }
 
 export default Footer
